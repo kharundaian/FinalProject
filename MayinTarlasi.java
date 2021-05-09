bb7c32638627a831349a99737fb4d6a5640106cf
zafyrd <73571159+zafyrd@users.noreply.github.com>
2021-05-10 01:30:37 +0600

    Update MayinTarlasi.java

--- MayinTarlasi.java
+++ MayinTarlasi.java
@@ -1,7 +1,6 @@
 import java.awt.GridLayout;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
