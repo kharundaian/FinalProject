36c63ff42d576def1dbc6172d5373049abfdfdac
Kharun Daian <73739781+kharundaian@users.noreply.github.com>
2021-05-09 23:08:10 +0600

    Update MayinTarlasi.java

--- MayinTarlasi.java
+++ MayinTarlasi.java
@@ -143,25 +143,22 @@ public class MayinTarlasi implements MouseListener {
 
     @Override
     public void mouseEntered(MouseEvent arg0) {
-        // TODO Auto-generated method stub
 
     }
 
     @Override
     public void mouseExited(MouseEvent arg0) {
-        // TODO Auto-generated method stub
 
     }
 
     @Override
     public void mousePressed(MouseEvent arg0) {
-        // TODO Auto-generated method stub
 
     }
 
     @Override
     public void mouseReleased(MouseEvent arg0) {
-        // TODO Auto-generated method stub
+
 
     }
 }
